3ffe258ad7ad0fc59d70cee4e8962d10f09e1e5a
Ivan <evanvaha@gmail.com>
2017-08-17 15:30:54 +0300

    init comit

---
+++ src/SendEmail.java
@@ -0,0 +1,66 @@
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+public class SendEmail
+{
+
+    public static void main(String[] args) throws MessagingException {
+        String host ="smtp.gmail.com";
+        String port = "587";
+        String userName= "evanvaha@gmail.com";//change for working
+        String password= "password";//change for working
+        String subject= "Hello";
+        String message= "Test mail";
+        String to = "evanvaha@gmail.com";
+        sendPlainTextEmail(host,port,userName,password,to,subject,message);
+    }
+
+
+    public static void sendPlainTextEmail(String host, String port,
+                                   final String userName, final String password, String toAddress,
+                                   String subject, String message) throws AddressException,
+            MessagingException {
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+// *** BEGIN CHANGE
+        properties.put("mail.smtp.user", userName);
+
+        // creates a new session, no Authenticator (will connect() later)
+        Session session = Session.getDefaultInstance(properties);
+// *** END CHANGE
+
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        // set plain text message
+        msg.setText(message);
+
+// *** BEGIN CHANGE
+        // sends the e-mail
+        Transport t = session.getTransport("smtp");
+        t.connect(userName, password);
+        t.sendMessage(msg, msg.getAllRecipients());
+        t.close();
+// *** END CHANGE
+
+    }
+
+
+
+}
